cdb218334641383a0137eb26e8864a981909f2bc
Amiya-873 <71648041+Amiya-873@users.noreply.github.com>
2022-06-20 20:02:42 +0530

    Update UCDeploySite.java

--- src/main/java/com/urbancode/jenkins/plugins/ucdeploy/UCDeploySite.java
+++ src/main/java/com/urbancode/jenkins/plugins/ucdeploy/UCDeploySite.java
@@ -52,6 +52,8 @@ public class UCDeploySite implements Serializable {
     private Secret password;
 
     private boolean trustAllCerts;
+    
+    public static boolean skipProps;
 
     private boolean alwaysCreateNewClient;
     
